3509fff607e7766327e6b4311ffb69c38e28d9e1
Billy J. Beltran <billy@redpill-linpro.com>
2017-06-27 15:49:06 +0200

    Added comments

--- bc.rb
+++ bc.rb
@@ -1,21 +1,21 @@
 module Bytecode
   
-  NOP = 0
-  JMP = 1
-  PEEK = 3
+  NOP = 0 # no op
+  JMP = 1 # jmp to instr spec. in immed byte
+  PEEK = 3 # stack peek
   CRAY = 7 #shuffle the next four instructions
-  CONS = 13
-  EXIT = 15
+  CONS = 13 # push immed byte onto stack
+  EXIT = 15 # exit happily
 
   #stack effects
-  SWAP = 2
-  DEC = 4
-  POP = 5
-  ADD = 6
-  SUB = 8
-  MUL = 10
-  DIV = 12
-  PRINT = 14
-  TEXT = 10
+  SWAP = 2 # switcharoo
+  DEC = 4 # DEPRECATED: decrease by 1, use CONST, 1, SUB
+  POP = 5 # ciao
+  ADD = 6 # sum top two
+  SUB = 8 # sub top two
+  MUL = 10 # ...
+  DIV = 12 # ..
+  PRINT = 14 # print int->to_s
+  TEXT = 10 # print int as char
 
 end
