2f51d66932e63ae2be56443e2892703245ba182e
chrysophylax <billy@redpill-linpro.com>
2018-01-15 15:05:26 +0100

    Extracting debugger to own file

--- vm.rb
+++ vm.rb
@@ -1,143 +1,137 @@
 #!/usr/bin/env ruby
 require_relative "bc"
 require_relative "m"
+require_relative "messages"
+require_relative "rf"
+
 
 class VirtualMachine
-  require 'io/console'
-
-  @@ssep = "\t\t\t\t\t\t"
-
-  def handle_key
-    STDIN.echo = false
-    STDIN.raw!
-    c = STDIN.read_nonblock(1) rescue nil
-    STDIN.cooked!
-    return c
-  end
-
-  def handle_input
-    k =  handle_key
-    case k
-
-    when "p"
-      puts "#{@@ssep} stack top -> #{i_to_hex(@stack.last)}."
-      
-    when "d"
-      # toggle debug
-      if @debug then @debug = false else @debug = true end
-      
-    when "h"
-      @running = false
-    when "i"
-      puts "#{@@ssep} instruction pointer is: #{@isp} -> #{i_to_hex(peek_fetch)}."
-    when "f"
-      puts "#{@@ssep} step increased."
-      step()
-    when "b"
-      puts "#{@@ssep} step decreased."
-      step_back()
-    when "x"
-      interpret()  
-    when "r"
-      if @running == false then
-        puts "#{@@ssep} resuming execution."
-        sleep 1
-        @running = true
-      end
-      
-    when "q"
-      @looping = false
-    end
-    
-    if @running
-      interpret()
-    end
-  end
+  include Inspector
+  attr_accessor :instructions, :interpreting, :running,
+                :max_stack_depth, :memory
+  attr_reader :stack_ptr, :isp, :cycles, :max_stack_depth, :stack
   
-
-
   def initialize()
+    @max_stack_depth = 100
     @instructions = Array.new
     @memory = Memory.new
-    @returns = Array.new
+    @returns = Array.new(size=100, default=0)
     @cycles = 0
-    @stack = Array.new
-    @stack.push(0)
+    @stack = Array.new(size=100, default=0)
     @isp = 0
     @debug = false
-    @running = false # i.e. interpreting
-    @looping = false # main program operation
+    @interactive = false
+    @interpreting = false # i.e. interpreting
+    @running = true # main program operation
+    @stack_ptr = -1 # "last element"
+
+  end
+
+  
+  def halt_vm()
+    @running = false
+    puts "Halting vardi..."
+  end
+  def start_vm()
+    @running = true
+    program_loop
+  end
+
+  def start_interpreter()
+    @interpreting = true
+    program_loop
+  end
+  
+  def halt_interpreter()
+    @interpreting = false
+  end
+
+  
+  def set_stack_ptr val 
+    @stack_ptr = val unless val == @max_stack_depth
   end
   
   def pretty_print_instructions
-    puts "\t\t --- instructions --- \n" + @instructions.to_s
+    puts VMInfo::PP_INSTRUCTIONS + @instructions.to_s
   end
   
   def step
-    @isp +=1
+    @isp +=1 
   end
   
   def step_back
     @isp -= 1 unless @isp == 0
   end
 
-  def peek_fetch
-    val = @instructions[@isp]
-    return val
-  end
-  
   def fetch()
+    if  @isp == @instructions.length then return -1 end
     val = @instructions[@isp]
     step()
     return val
+  end
+
+  def peek_instr
+    if  @isp == @instructions.length then return -1 end
+    val = @instructions[@isp]
+    return val 
   end
 
   def i_to_hex i
     return i.to_s(16).upcase
   end
 
+  def bytecode_exit
+    if @debug then debug_bytecode_exit end
+    halt_interpreter()
+    halt_vm() unless @interactive
+  end
+    
   def interpret()
     @cycles += 1
-    
+
     #fetch
     instr = fetch()
 
     #decode
     case instr
+    when -1
+      puts "Overran!"
+      bytecode_exit
     when Bytecode::NOP
-      if @debug then puts "\tNOP" end
+      if @debug then debug_bytecode_nop end
       
     when Bytecode::PEEK
-      if @debug then puts "\tPEEK" end
+      if @debug then debug_bytecode_peek end
       puts @stack.last.to_s
 
     when Bytecode::PRINT
-      if @debug then puts "\tPRINT.#{@stack.last.to_s}" end
+      if @debug then debug_bytecode_print  end
       print @stack.pop.to_s
 
     when Bytecode::CONS
       a = fetch()
       @stack.push(a)
-      if @debug then puts "\tCONS.#{@stack.last.to_s}" end
+      if @debug then debug_bytecode_cons end
+      #if @debug then puts "\tCONS.#{@stack.last.to_s}" end
       
     when Bytecode::POP
-      if @debug then puts "\tPOP" end
+      if @debug then debug_bytecode_pop end
       @stack.pop()
       
     when Bytecode::JMP
-      if @debug then puts "\tJMP" end
+      if @debug then debug_bytecode_jmp end
       addr = fetch()
       @isp = addr
 
     when Bytecode::JMS
-      if @debug then puts "\tJMS" end
+      if @debug then debug_bytecode_jms end
       upper = @stack.pop().to_s(16)
       lower = @stack.pop().to_s(16)
       addr = (upper + lower).to_i(16)
       @isp = addr
 
     when Bytecode::JIZ
-      if @debug then puts "\tJIZ" end
+      if @debug then debug_bytecode_jiz end
       v = @stack.last()
       if v == 0
         addr = fetch()
@@ -149,11 +143,11 @@ class VirtualMachine
       end
       
     when Bytecode::TEXT
-      if @debug then puts "\tTEXT.#{@stack.last.chr}" end
+      if @debug then debug_bytecode_text end
       print @stack.pop.chr
       $stdout.flush()
     when Bytecode::AND
-      if @debug then puts "\tAND" end
+      if @debug then debug_bytecode_and end
       #pop, AND, push
       a = @stack.pop()
       b = @stack.pop()
@@ -161,79 +155,86 @@ class VirtualMachine
       @stack.push(c)
       
     when Bytecode::NOT
+      if @debug then debug_bytecode_not end
+      
     when Bytecode::SWAP
-      if @debug then puts "\tSWAP" end
+      if @debug then debug_bytecode_swap end
       last = @stack.pop()
       older = @stack.pop()
       @stack.push(last).push(older)
 
     when Bytecode::DUP
-      if @debug then puts "\tDUP" end
+      if @debug then debug_bytecode_dup end
       @stack.push(@stack.last)
       
     when Bytecode::RSWP
-      if @debug then puts "\tRSWP" end
+      if @debug then debug_bytecode_rswp end
       addr = @returns.pop()
       val = @stack.pop()
       @returns.push(val)
       @stack.push(addr)
 
     when Bytecode::ISWP
-      if @debug then puts "\tISWP.*0x#{@instructions[@isp].to_s(16)}<-0x#{@stack.last.to_s(16)}" end
+#      if @debug then puts "\tISWP.*0x#{@instructions[@isp].to_s(16)}<-0x#{@stack.last.to_s(16)}" end
+      if @debug then debug_bytecode_iswp end
       instr = @instructions[@isp]
       val = @stack.pop()
       @stack.push(instr)
       @instructions[@isp] = val
       
     when Bytecode::DEC
-      if @debug then puts "\tDEPRECATED: DEC" end
+      if @debug then debug_bytecode_dec end
       a = @stack.pop()
       @stack.push(a-1)
       
     when Bytecode::ADD
-      if @debug then puts "\tADD" end
+      if @debug then debug_bytecode_add end
       a = @stack.pop()
       b = @stack.pop()
       @stack.push(b+a)
-      
+
     when Bytecode::SUB
-      if @debug then puts "\tSUB" end
+      if @debug then debug_bytecode_sub end
       a = @stack.pop()
       b = @stack.pop()
       @stack.push(b-a)
       if @debug then puts "RES: #{@stack.last()}"end
       
     when Bytecode::MUL
-      if @debug then puts "\tMUL" end
+      if @debug then debug_bytecode_mul end
       a = @stack.pop()
       b = @stack.pop()
       @stack.push(b*a)
       
     when Bytecode::DIV
-      if @debug then puts "\tDIV" end 
+      if @debug then debug_bytecode_div end
       a = @stack.pop()
       b = @stack.pop()
       @stack.push(b/a)
       
     when Bytecode::CRAY
     #TODO: Malbolge crazy operator
+      if @debug then debug_bytecode_cray end
       
     when Bytecode::JVM
-      if @debug then puts "\t***COFFEETIME***" end
+      #if @debug then puts "\t***COFFEETIME***" end
+      if @debug then debug_bytecode_jvm end
       duration = fetch()
       sleep(duration)
       
     when Bytecode::EXIT
-      if @debug then puts "\tEXIT" end
-      @running = false
+      if @debug then debug_bytecode_exit end
+      bytecode_exit
       
     when Bytecode::RET
-      if @debug then puts "\tRET.0x#{@returns.last.to_s(16)}" end
+      if @debug then debug_bytecode_ret end
+#     if @debug then puts "\tRET.0x#{@returns.last.to_s(16)}" end 
       addr = @returns.pop()
       @isp = addr
       
     when Bytecode::CALL
-      if @debug then puts "\tCALL.*0x#{@instructions[@isp].to_s(16)}<-0x#{(@isp-1).to_s(16)}" end
+#      if @debug then puts "\tCALL.*0x#{@instructions[@isp].to_s(16)}<-0x#{(@isp-1).to_s(16)}" end
+      if @debug then debug_bytecode_call end
       addr = fetch()
       current = @isp
       @isp = addr
@@ -265,32 +266,41 @@ class VirtualMachine
       
     else
       #curse programmer in hex
-      puts "#{@@ssep}Could not understand  #{@isp}: instr. was #{i_to_hex(peek_fetch)}."
+      puts VMInfo::MAL_INSTR
+      puts "#{@isp} -> [#{i_to_hex ( @instructions[@isp] ) }]."
       @running = false
     end
 
-    
+
+    if (@isp == @instructions.length) then halt_interpreter() end 
     # Let us exit even if programmer forgot to 0x1111
     #if @instructions.length <= @isp then @running = false end
   end
   
   
   def set_flags
-    if ARGV[1].to_s == "d"
+    if ARGV.include?("-d")
       @debug = true
     end
-  end
-
-  def run_loop
-    while @running
-      interpret()
+    if ARGV.include?("-i")
+      @interactive = true
     end
+    if ARGV.include?("-h")
+      halt_interpreter()
+    end
+
   end
 
   def program_loop
-    while @looping
+   while @running
+    if @interactive
       handle_input
     end
+    if @interpreting
+      interpret()
+    end
+   end
+   
   end
   
   def load_file
@@ -305,15 +315,14 @@ class VirtualMachine
   def print_stats()
     puts "Program length: #{@instructions.length} bytes. Executed #{@cycles} cycles."
   end
+
   def start
     set_flags
     load_file
     if @debug
       pretty_print_instructions()
     end
-    @looping = true
-    puts "vardi>"
-    program_loop
+    start_interpreter()
     if @debug
       print_stats()
     end
